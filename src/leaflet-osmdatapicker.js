09662d9caf4ca3df55d95b603b071274cbfefbc7
m2cci-hamadeha <amr.grenoble@gmail.com>
2024-01-28 00:35:14 +0100

    :sparkles: Dialog form added with style

--- src/leaflet-osmdatapicker.js
+++ src/leaflet-osmdatapicker.js
@@ -1,12 +1,4 @@
-(function (factory, window) {
-  if (typeof define === "function" && define.amd) {
-    define(["leaflet"], factory);
-  } else if (typeof exports === "object") {
-    module.exports = factory(require("leaflet"));
-  } else {
-    window.L.OSMDataPicker = factory(L);
-  }
-})(function (L) {
+(function (L) {
   var OSMDataPicker = {};
 
   OSMDataPicker.addControl = function () {
@@ -16,10 +8,10 @@
       },
       onAdd: function (map) {
         var button = L.DomUtil.create("button", "leaflet-osmdatapicker-button");
-        button.innerHTML = "OSM Button";
+        button.innerHTML = "Open OSM Data Picker";
 
         L.DomEvent.on(button, "click", function () {
-          console.log("Clicked");
+          openOSMDataPickerDialog();
         });
 
         return button;
@@ -28,9 +20,49 @@
 
     var control = new customControl();
     control.addTo(map);
+
+    // Create the dialoge form
+    var dialog = L.DomUtil.create("div", "leaflet-osmdatapicker-dialog");
+    dialog.innerHTML = `
+          <div class="leaflet-osmdatapicker-header">
+              <h2>OSM Data Picker</h2>
+          </div>
+          <div class="leaflet-osmdatapicker-content">
+              <h4>Choose an amenity :</h4>
+              <select id="osmDataPickerSelect">
+                  <option value="value1">V1</option>
+                  <option value="value2">V2</option>
+              </select>
+          </div>
+          <div class="leaflet-osmdatapicker-footer">
+              <button id="osmDataPickerSubmit">Validate</button>
+              <button id="osmDataPickerCancel">Cancel</button>
+          </div>
+      `;
+    map.getContainer().appendChild(dialog);
+
+    dialog.style.display = "none";
+
+    var submitButton = dialog.querySelector("#osmDataPickerSubmit");
+    submitButton.addEventListener("click", function () {
+      console.log("Validated");
+    });
+
+    var cancelButton = dialog.querySelector("#osmDataPickerCancel");
+    cancelButton.addEventListener("click", function () {
+      console.log("Canceled");
+      closeOSMDataPickerDialog();
+    });
+
+    function openOSMDataPickerDialog() {
+      dialog.style.display =
+        dialog.style.display === "block" ? "none" : "block";
+    }
+
+    function closeOSMDataPickerDialog() {
+      dialog.style.display = "none";
+    }
   };
 
   L.OSMDataPicker = OSMDataPicker;
-
-  return OSMDataPicker;
-}, window);
+})(window.L);
