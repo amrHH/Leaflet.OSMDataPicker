e7f9c741ac0aa8471dd7e31a4dac5f1a0522e1f4
m2cci-hamadeha <amr.grenoble@gmail.com>
2024-02-07 17:18:51 +0100

    :sparkles: adding json file containing all osm tags and values + reading it in the dialog

--- src/leaflet-osmdatapicker.js
+++ src/leaflet-osmdatapicker.js
@@ -11,7 +11,6 @@
         button.innerHTML = "Open OSM Data Picker";
         L.DomEvent.on(button, "click", function (e) {
           L.DomEvent.stopPropagation(e);
-
           L.DrawingFunctions.startDrawing(map, function (drawnPolygon) {
             openOSMDataPickerDialog();
             console.log("Polygon drawn:", drawnPolygon);
@@ -25,17 +24,19 @@
     var control = new customControl();
     control.addTo(map);
 
-    // Create the dialoge form
+    // Create the dialog form
     var dialog = L.DomUtil.create("div", "leaflet-osmdatapicker-dialog");
     dialog.innerHTML = `
           <div class="leaflet-osmdatapicker-header">
               <h2>OSM Data Picker</h2>
           </div>
           <div class="leaflet-osmdatapicker-content">
-              <h4>Choose an amenity :</h4>
-              <select id="osmDataPickerSelect">
-                  <option value="value1">V1</option>
-                  <option value="value2">V2</option>
+              <h4>Choose a category :</h4>
+              <select class = "osmListBox" id="KeysList">
+              <option value=""></option>
+              </select>
+              <h4>Choose a value :</h4>
+              <select class = "osmListBox" id="ValuesList">
               </select>
           </div>
           <div class="leaflet-osmdatapicker-footer">
@@ -66,6 +67,48 @@
     function closeOSMDataPickerDialog() {
       dialog.style.display = "none";
     }
+
+    // Load JSON file and populate KeysList
+    fetch("./assets/osmtags.json")
+      .then((response) => response.json())
+      .then((data) => {
+        var keyList = document.getElementById("KeysList");
+        Object.keys(data).forEach((key) => {
+          var option = document.createElement("option");
+          option.value = key;
+          option.textContent = key;
+          keyList.appendChild(option);
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+    // Add event listener for KeysList change
+    var keysList = document.getElementById("KeysList");
+    keysList.addEventListener("change", function () {
+      var selectedKey = keysList.value;
+      var valuesList = document.getElementById("ValuesList");
+      valuesList.innerHTML = "";
+
+      // Populate ValuesList based on selectedKey
+      fetch("./assets/osmtags.json")
+        .then((response) => response.json())
+        .then((data) => {
+          var values = data[selectedKey];
+          if (values) {
+            values.forEach((value) => {
+              var option = document.createElement("option");
+              option.value = value;
+              option.textContent = value;
+              valuesList.appendChild(option);
+            });
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
   };
 
   L.OSMDataPicker = OSMDataPicker;
