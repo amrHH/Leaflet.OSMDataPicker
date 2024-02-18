295cc9535886da2f83d0af43d38d5dd09c722f63
m2cci-hamadeha <amr.grenoble@gmail.com>
2024-02-18 23:45:24 +0100

    Fix js file 2

--- src/leaflet-osmdatapicker.js
+++ src/leaflet-osmdatapicker.js
@@ -139,7 +139,7 @@
     }
 
     // Load JSON file and populate KeysList
-    fetch("./src/assets/osmtags.json")
+    fetch(".Leaflet.OSMDataPicker/src/assets/osmtags.json")
       .then((response) => response.json())
       .then((data) => {
         var keyList = document.getElementById("KeysList");
@@ -162,7 +162,7 @@
       valuesList.innerHTML = "";
 
       // Populate ValuesList based on selectedKey
-      fetch("./src/assets/osmtags.json")
+      fetch(".Leaflet.OSMDataPicker/src/assets/osmtags.json")
         .then((response) => response.json())
         .then((data) => {
           var values = data[selectedKey];
